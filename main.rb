1a85bf5b1f353142c4013a99937c1539be00bb79
dave2281 <dava.bigboy@gmail.com>
2023-01-25 19:14:44 +0200

    Modified main.rb

--- main.rb
+++ main.rb
@@ -10,19 +10,15 @@ class Recipes
 
   def find_recipe(user_input)
     @arr << user_input
-    p @arr
     @arr.map do |input|
       @ingreds << input
-      p @ingreds
     end
     @recipes.map do |recipes_and_keys|
       @ingreds.map do |ingred|
         if recipes_and_keys[0].include?(ingred)
           unless user_input.nil? || user_input.empty?
             @arr2 << recipes_and_keys[1]
-            p @arr2
             $recipes2[user_input] = @arr2
-            p $recipes2
           else
             break
           end
